3bcdb91078611ce8d630e99ffad1baa5a7b9a51e
LouvartY <152400789+LouvartY@users.noreply.github.com>
2023-11-29 14:09:35 +0100

    Add files via upload

---
+++ Question1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[]){
+	const char *message = "\nBienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n";
+	write(1, message, strlen(message));
+
+	return 0;
+}
