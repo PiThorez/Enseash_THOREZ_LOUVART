ed1c488ef39a5fc4b506f3e2473e362b845c4b25
PiThorez <pierre.thorez@ensea.fr>
2023-11-29 16:04:16 +0100

    Create Question3.c

---
+++ Question3.c
@@ -0,0 +1,36 @@
+#include "Questions.h"
+
+#define TAILLE 128
+
+
+
+char cmd_read[TAILLE];
+ssize_t cmd_size;
+pid_t pid;
+int status;
+
+
+int main(int argc, char const *argv[]){
+    
+	while(1){
+        const char *message = "\nBienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\nenseash %\n";
+	    write(STDOUT_FILENO, message, strlen(message));
+        cmd_size = read(STDIN_FILENO,cmd_read,TAILLE);
+        cmd_read[cmd_size-1] = '\0';
+
+        if(strcmp(cmd_read,"exit")==0){
+            const char * message_exit = "\n Bye Bye ...\n\n";
+            write(STDOUT_FILENO, message_exit, strlen(message_exit));
+            exit(1);
+        }
+
+        pid = fork();
+        if(pid != 0){
+            wait(&status);
+        }
+        else{
+            execlp(cmd_read,cmd_read,(char*)NULL);
+        }
+
+    }
+}
