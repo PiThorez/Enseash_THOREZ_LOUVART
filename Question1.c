9fee81a13bf14c7722bc9a3b74fbeefd3e473078
PiThorez <pierre.thorez@ensea.fr>
2023-11-29 14:12:27 +0100

    Update Question1.c

--- Question1.c
+++ Question1.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 
 int main(int argc, char const *argv[]){
-	const char *message = "\nBienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n";
+	const char *message = "\nBienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\nenseash\n";
 	write(1, message, strlen(message));
 
 	return 0;
