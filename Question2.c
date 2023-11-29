72e63f748f8f1ec4e8b88a57f7adb56e3ecd0a04
PiThorez <pierre.thorez@ensea.fr>
2023-11-29 15:40:49 +0100

    Debut question 2

--- Question2.c
+++ Question2.c
@@ -1,26 +1,25 @@
 #include "Questions.h"
 
+#define TAILLE 128
+
+
+
+char cmd_read[TAILLE];
+ssize_t cmd_size;
+pid_t pid;
+
+
 int main(int argc, char const *argv[]){
-    const char *message = "\nBienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\nenseash %\n";
-	write(1, message, strlen(message));
-	
-	const char *messageRead = "\0";
-	const char *command = "date";
-	const char *commandExit = "exit";
+    
+	while(1){
+        const char *message = "\nBienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\nenseash %\n";
+	    write(1, message, strlen(message));
 
-
-    if(messageRead == "exit"){
-    	write(1,"Bye bye...\n",strlen("Bye bye...\n"));
-    	execlp(commandExit,commandExit,(char *)NULL);
+        pid = fork();
+        if(pid != 0){
+            cmd_size = read(STDOUT_FILENO,cmd_read,TAILLE);
+            execlp(cmd_read,cmd_read,(char*)NULL);
+        }
 
     }
-
-    else if(messageRead == "\0"){
-    	execlp(command,command, (char *)NULL);
-    }
-
-    else{
-    	write(1,"oops",strlen("oops"));
-    }
-    return 0;
 }
--- README.md
+++
@@ -1,1 +0,0 @@
-# Enseash_THOREZ_LOUVART
